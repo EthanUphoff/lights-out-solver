c4efd9cf3bfc6706859648065532d86a8897501b
EthanUphoff <ethan.uphoff@gmail.com>
2019-11-13 15:19:02 -0600

    Change to lights out.

--- index.js
+++ index.js
@@ -1,23 +1,73 @@
-let sudoku = require('sudoku')
-let children = []
-let sudokumap = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9}
-let sudokutest = sudoku.makepuzzle().map(x => x === null ? sudokumap : x+1)
+// false = on
+const lightout = [[false, false, false, false, false], [false, false, false, false, false], [false, false, false, false, false], [false, false, false, false, false], [false, false, false, false, false]]
+const truearr = [[true, true, true, true, true], [true, true, true, true, true], [true, true, true, true, true], [true, true, true, true, true], [true, true, true, true, true]]
+const posarr = [[0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0]]
 
-const depthfirst = (children) => {
-    children.unshift(children)
-}
-
-const solve = () => {
-    for (i = 0; i < sudokutest.length; i++){
-        if ((typeof x) !== 'number'){
-            
-        }
+validate = (l) => {
+  let i = 0;
+  do {
+    if(l[i].includes(false)){
+      console.log(false)
+      return
     }
-    console.log(sudokutest)
+    i++
+  } while (i < 5)
+  console.log(true)
 }
 
-const checkrowsandcolumns = (index, node) => {
-    const rowpos = index - (index % 9)
+// for every true, check the number of trues around it, prioritize trues with more values around them.
+// return negative if x < y, positive x > y, 0 x=y
+comparator = (x, y) => {
+
 }
 
-solve()
+solve = () => {
+  let posToCareAbout = [];
+  for (i = 0; i < lightout.length; i ++) {
+    for (j = 0; j < lightout[i].length; j++) {
+      if (!lightout[i][j]) {
+        insertIntoPosArr(posToCareAbout, [[i, j], generateVal(i, j)])
+      }
+    }
+  }
+}
+
+insertIntoPosArr = (arr, pos) => {
+  let returnArr = [];
+  if (arr.length == 0) {
+    arr[0] = pos;
+    return;
+  }
+  let i;
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i][1] < pos[1]) {
+      returnArr.push(pos)
+      break
+    } else {
+      returnArr.push(arr[i])
+    }
+  }
+  for (i = i; i < arr.length; i++) {
+    returnArr.push(arr[i])
+  }
+  return returnArr
+}
+
+generateVal = (i, j) => {
+  let val = 1;
+  lightout[i-1] && lightout[i-1][j] && !lightout[i-1][j] ? val++ : undefined
+  lightout[i+1] && lightout[i+1][j] && !lightout[i+1][j] ? val++ : undefined
+  lightout[i] && lightout[i][j-1] && !lightout[i][j-1] ? val++ : undefined
+  lightout[i] && lightout[i][j+1] && !lightout[i][j+1] ? val++ : undefined
+  return val
+}
+
+switching = (i, j) => {
+  lightout[i-1] && lightout[i-1][j] && !lightout[i-1][j] ? val++ : undefined
+  lightout[i+1] && lightout[i+1][j] && !lightout[i+1][j] ? val++ : undefined
+  lightout[i] && lightout[i][j-1] && !lightout[i][j-1] ? val++ : undefined
+  lightout[i] && lightout[i][j+1] && !lightout[i][j+1] ? val++ : undefined
+}
+
+validate(lightout)
+validate(truearr)
